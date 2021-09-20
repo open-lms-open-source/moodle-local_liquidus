592fa100b18f918da6afa608be97a48326c06c9c
Fabian Batioja <fabian.batioja@openlms.net>
2021-09-20 16:03:46 +0000

    INT-17437: Bump versions for 3.11: After merge

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 /** @var object $plugin */
 $plugin->version   = 2021060109; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020110900; // Requires this Moodle version.
+$plugin->requires  = 2021051700; // Requires this Moodle version.
 $plugin->component = 'local_liquidus'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.10.3';
+$plugin->release   = '3.11.2';
