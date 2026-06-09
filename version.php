d8e1dc798e4db688aa5d7e955a777871ac55ea7e
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2026-06-09 15:27:20 -0500

    INT-21630: 5.1 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 /** @var object $plugin */
-$plugin->version = 2026032600; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2024100700; // Requires this Moodle version.
+$plugin->version = 2026060900; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires = 2025100600; // Requires this Moodle version.
 $plugin->component = 'local_liquidus'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.5.4';
+$plugin->release = '5.1.4';
